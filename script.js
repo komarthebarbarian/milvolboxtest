594c6706b0ee156ea5dd9afec4c0b6cf8528703b
komarthebarbarian <ajdukmare@gmail.com>
2023-04-03 19:48:14 +0200

    Alfa faza

--- script.js
+++ script.js
@@ -15,3 +15,14 @@ $(".carousel.carousel-slider").carousel({
   fullWidth: true,
   indicators: true,
 });
+
+$(document).ready(function () {
+  $(".materialboxed").materialbox();
+});
+
+$('[lang="en"]').hide();
+$('[lang="sr"]').show();
+$(".language").click(function () {
+  $("*:lang(sr)").toggle();
+  $("*:lang(en)").toggle();
+});
